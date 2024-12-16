53a789e73f82e953872d0a4d72983e7cbbe75ab8
София Бердникова <sofiytula71@gmail.com>
2024-12-16 12:12:05 +0300

    working with the project structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"github.com/bulbosaur/web-calculator/internal/application"
+)
+
+func main() {
+	app := application.New()
+	app.RunServer
+}
---
+++ internal/application/application.go
@@ -0,0 +1,25 @@
+package application
+
+import (
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	Addr string
+}
+
+func GettingConfig() *Config {
+	config := new(Config)
+	address := os.Getenv("PORT")
+	config.Addr = ":" + address
+	if config.Addr == "" {
+		config.Addr = ":8080"
+	}
+	return config
+}
+
+func RunServer() error {
+	http.HandleFunc("/", CalcHandler)
+
+}
